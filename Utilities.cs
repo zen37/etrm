3efce055e2ef892e28a0c400b43604d6119e5158
zen <zen@talk-talk.net>
2024-07-06 18:06:07 -0700

    optional param

--- Utilities.cs
+++ Utilities.cs
@@ -1,3 +1,4 @@
+using System.Runtime.InteropServices;
 using Axpo;
 
 public static class Utilities
@@ -6,7 +7,7 @@ public static class Utilities
     {
         TimeZoneInfo timezone = TimeZoneInfo.FindSystemTimeZoneById(timeZoneId);
         DateTime dateToRetrieve = TimeZoneInfo.ConvertTime(DateTime.UtcNow, timezone);
-        
+
         string formattedDateTime = dateToRetrieve.ToString(format);
 
         return formattedDateTime;
@@ -17,20 +18,25 @@ public static class Utilities
 
         TimeZoneInfo timezone = TimeZoneInfo.FindSystemTimeZoneById(timeZoneId);
         DateTime dateToRetrieve = TimeZoneInfo.ConvertTime(DateTime.UtcNow, timezone);
-     
+
         return dateToRetrieve;
     }
 
-  public static void PrintTrades(IEnumerable<PowerTrade> trades)
+    public static void PrintTrades(IEnumerable<PowerTrade> trades, int counterPeriods = 0)
     {
 
         foreach (PowerTrade trade in trades)
         {
             Console.WriteLine($"Trade ID: {trade.TradeId}");
-
+            int i = 0;
             foreach (PowerPeriod period in trade.Periods)
             {
                 Console.WriteLine($"Period {period.Period}: Volume: {period.Volume}");
+                i += 1;
+                if (i > counterPeriods && counterPeriods > 0)
+                {
+                    break;
+                }
             }
         }
     }
